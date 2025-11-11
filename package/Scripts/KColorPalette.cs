54823ca22b0061a8212c517b7ffd2e8a192bb64c
luan.votrong.gpronet <luan.votrongdev@gmail.com>
2025-11-11 14:38:04 +0900

    Update namespace

---
+++ package/Scripts/KColorPalette.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace Khoai
+{
+    [CreateAssetMenu(menuName = "UI/KColorPalette")]
+    public class KColorPalette : ScriptableObject
+    {
+        public event Action Changed;
+
+        public List<Color> colors = new();
+
+    #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            Changed?.Invoke();
+        }
+    #endif
+    }
+}
---
+++ package/Scripts/KUIColorBinder.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+namespace Khoai
+{
+    public class ColorSelectorAttribute : PropertyAttribute { }
+
+    [ExecuteAlways]
+    public class KUIColorBinder : MonoBehaviour
+    {
+        public KColorPalette palette;
+        public Component target;
+        [ColorSelector] public int color;
+
+        void OnValidate()
+        {
+            if (!target)
+            {
+                target = GetComponent<Graphic>();
+                if (!target) target = GetComponent<TMP_Text>();
+            }
+            UpdateColor();
+        }
+
+        void OnEnable()
+        {
+#if UNITY_EDITOR
+            if (palette) palette.Changed += UpdateColor;
+#endif
+            UpdateColor();
+        }
+
+        void OnDisable()
+        {
+#if UNITY_EDITOR
+            if (palette) palette.Changed -= UpdateColor;
+#endif
+        }
+
+        void UpdateColor()
+        {
+            if (!palette || !target) return;
+            var c = palette.colors[color];
+            if (target is Graphic g) g.color = c;
+            else if (target is TMP_Text t) t.color = c;
+            else Debug.LogWarning($"{target.name} has no color property.", this);
+        }
+    }
+}

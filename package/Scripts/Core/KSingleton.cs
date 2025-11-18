4c13d63ac260787f39f7c93129ce7fcb75174444
luan.votrong.gpronet <luan.votrongdev@gmail.com>
2025-11-18 10:30:51 +0900

    Runnable sprite binding

--- package/Scripts/Core/KSingleton.cs
+++ package/Scripts/Core/KSingleton.cs
@@ -1,5 +1,5 @@
+using System;
 using UnityEngine;
-
 
 namespace Khoai
 {
@@ -7,7 +7,7 @@ namespace Khoai
 	{
 		// Check to see if we're about to be destroyed.
 		private static bool m_ShuttingDown;
-		private static object m_Lock = new();
+		private static readonly object m_Lock = new();
 		private static T m_Instance;
 
 		/// <summary>
@@ -26,23 +26,22 @@ namespace Khoai
 
 				lock (m_Lock)
 				{
+					if (m_Instance != null) return m_Instance;
+
+					// Search for existing instance.
+					m_Instance = (T)FindFirstObjectByType(typeof(T));
+
+					// Create new instance if one doesn't already exist.
 					if (m_Instance == null)
 					{
-						// Search for existing instance.
-						m_Instance = (T)FindFirstObjectByType(typeof(T));
-
-						// Create new instance if one doesn't already exist.
-						if (m_Instance == null)
-						{
-							// Need to create a new GameObject to attach the singleton to.
-							var singletonObject = new GameObject();
-							m_Instance = singletonObject.AddComponent<T>();
-							singletonObject.name = typeof(T).ToString() + " (Singleton)";
-						}
-
-						// Make instance persistent.
-						DontDestroyOnLoad(m_Instance.gameObject);
+						// Need to create a new GameObject to attach the singleton to.
+						var singletonObject = new GameObject();
+						m_Instance = singletonObject.AddComponent<T>();
+						singletonObject.name = typeof(T).ToString() + " (Singleton)";
 					}
+
+					// Make instance persistent.
+					DontDestroyOnLoad(m_Instance.gameObject);
 
 					return m_Instance;
 				}
